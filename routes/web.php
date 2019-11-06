387e4c87a8c64e15a58d8bd88d2812887aaf315d
Mateus Torquato de Medeiros <matteus.torquato@gmail.com>
2019-11-06 16:09:00 -0300

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,15 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+$router->get('people', ['as' => 'api.people.index', 'uses' => 'PersonController@index']);
+$router->post('people', ['as' => 'api.people.store', 'uses' => 'PersonController@store']);
+$router->patch('people/{id}', ['as' => 'api.people.update', 'uses' => 'PersonController@update']);
+$router->put('people/{id}', ['as' => 'api.people.update', 'uses' => 'PersonController@update']);
+$router->delete('people/{id}', ['as' => 'api.people.destroy', 'uses' => 'PersonController@destroy']);
+
+$router->get('contacts', ['as' => 'api.contacts.index', 'uses' => 'ContactController@index']);
+$router->post('contacts', ['as' => 'api.contacts.store', 'uses' => 'ContactController@store']);
+$router->patch('contacts/{id}', ['as' => 'api.contacts.update', 'uses' => 'ContactController@update']);
+$router->put('contacts/{id}', ['as' => 'api.contacts.update', 'uses' => 'ContactController@update']);
+$router->delete('contacts/{id}', ['as' => 'api.contacts.destroy', 'uses' => 'ContactController@destroy']);
+
